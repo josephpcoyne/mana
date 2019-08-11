139ed83b3e56dd4c7db37858e245d2f02babe60b
josephpcoyne <josephpcoyne@gmail.com>
2019-08-11 00:26:48 -0400

    initial commit

--- src/main.js
+++ src/main.js
@@ -1,10 +1,15 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
+import Vue from "vue";
+import Vuex from "vuex";
+import App from "./App.vue";
+import router from "./router";
+import "es6-promise/auto";
+Vue.config.productionTip = false;
 
-Vue.config.productionTip = false
+axios.defaults.baseURL =
+  process.env.NODE_ENV === "development" ? "http://localhost:3000" : "/";
 
+Vue.use(Vuex);
 new Vue({
   router,
   render: h => h(App)
-}).$mount('#app')
+}).$mount("#app");
