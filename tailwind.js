88bc9424f906f5d369cafb56578a5f90da7365d1
Joseph Coyne <39542045+josephpcoyne@users.noreply.github.com>
2020-10-02 18:29:38 -0400

    Reverts tailwind.js

---
+++ tailwind.js
@@ -0,0 +1,17 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        mana: {
+          "100": "#e0f6ff",
+          "200": "#32c0fb"
+        }
+      }
+    },
+    variants: {
+      boxShadow: ['responsive', 'hover', 'focus', 'active'],
+      backgroundColor: ['responsive', 'hover', 'focus', 'active']
+    },
+    plugins: []
+  }
+};
