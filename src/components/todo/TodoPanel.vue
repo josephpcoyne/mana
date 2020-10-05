013dbffc6702e2c33377086004d451529f64b4e1
Joseph <joseph@coyne.dev>
2020-10-05 15:46:44 -0400

    adds working folder and sublist structure

--- src/components/todo/TodoPanel.vue
+++ src/components/todo/TodoPanel.vue
@@ -1,7 +1,7 @@
 <template>
   <section id="todo-panel">
 
-    <div class="mb-12 py-3">
+    <div class="mb-12 py-3 ">
 			<button
 				class="flex m-auto font-bold py-2 px-4 rounded-lg shadow active:shadow-inner hover:text-mana-200 active:text-blue-200 text-sm text-gray-500"
 			>New List</button>
@@ -9,21 +9,21 @@
     <ul class="border-b pb-6 border-gray-200">
       <li tabindex="0" class="group flex justify-between items-center h-8 rounded-lg px-2 my-1 cursor-pointer focus:bg-mana-100 ">
         <span class="text-gray-600 group-focus:font-sans-bold group-focus:text-gray-700">
-          <i class="fas fa-list-alt pr-4  text-gray-600  group-focus:text-mana-200"></i>
+          <i class="fas fa-list-alt pr-3  text-gray-600  group-focus:text-mana-200"></i>
           All
         </span>
         <span class="font-sans-bold text-gray-600  group-focus:text-gray-800">15</span>
       </li>
       <li tabindex="0" class="group flex justify-between items-center h-8 rounded-lg px-2 my-1 cursor-pointer focus:bg-mana-100 ">
         <span class="text-gray-600 group-focus:font-sans-bold group-focus:text-gray-700">
-          <i class="fas fa-calendar-day pr-4  text-gray-600  group-focus:text-mana-200"></i>
+          <i class="fas fa-calendar-day pr-3  text-gray-600  group-focus:text-mana-200"></i>
           Today
         </span>
         <span class="font-sans-bold text-gray-600  group-focus:text-gray-800">5</span>
       </li>
       <li tabindex="0" class="group flex justify-between items-center h-8 rounded-lg px-2 my-1 cursor-pointer focus:bg-mana-100 ">
         <span class="text-gray-600 group-focus:font-sans-bold group-focus:text-gray-700">
-          <i class="fas fa-calendar-week pr-4  text-gray-600  group-focus:text-mana-200"></i>
+          <i class="fas fa-calendar-week pr-3  text-gray-600  group-focus:text-mana-200"></i>
           This Week
         </span>
         <span class="font-sans-bold text-gray-600  group-focus:text-gray-800">10</span>
@@ -31,27 +31,53 @@
     </ul>
 
     <div class="py-3 mt-4 flex justify-between">
-      <span class="text-md text-gray-500">Lists </span>
+      <span class="text-md text-gray-500">Lists</span>
       <i class="fas fa-plus text-gray-500 text-sm p-1 pt-1 cursor-pointer hover:text-mana-200"></i>  
     </div>
 
     <ul v-for="list in lists" :key="list.id" @click="selected = list.id">
-      <li tabindex="0" :class="{ 'pl-8': list.subList }" class="group flex justify-between items-center h-8 rounded-lg px-2 my-1 cursor-pointer focus:bg-mana-100 ">
+      <li tabindex="0" :class="{ 'pl-8': list.subList }" 
+      class="group flex justify-between items-center h-8 rounded-lg px-2 my-1 cursor-pointer focus:bg-mana-100"
+      @click="rotate(list)">
         <span class="text-gray-600 group-focus:font-sans-bold group-focus:text-gray-700 truncate ...">
-          <i v-if="list.folder && !list.isOpen" class="fas fa-folder pr-4  text-gray-600  group-focus:text-mana-200"></i>
-          <i v-else-if="list.isOpen" class="fas fa-folder-open pr-4  text-gray-600  group-focus:text-mana-200"></i>
-          <i v-else class="fas fa-list-ul pr-4  text-gray-600  group-focus:text-mana-200 "></i>
+          <i v-if="list.folder && !list.isOpen" class="fas fa-folder pr-3  text-gray-600  group-focus:text-mana-200"></i>
+          <i v-else-if="list.isOpen" class="fas fa-folder-open pr-3  text-gray-600  group-focus:text-mana-200"></i>
+          <i v-else class="fas fa-list-ul pr-3  text-gray-600  group-focus:text-mana-200 "></i>
           {{list.title}}
         </span>
-        <div class="flex justify-between items-center">
-        <!-- <div class="inline-grid grid-cols-2 justify-items-center items-center"> -->
-          <span class="text-gray-600 group-focus:text-gray-800 m-1 ">
+        <div class="flex items-center pl-2">
+          
+          <span v-if="!list.folder" class="text-gray-600 group-focus:text-gray-800">
             {{list.tasks}}
           </span>
-            <i v-if="list.isOpen" class="fas fa-chevron-down fa-fw" ></i>
-            <i v-if="!list.isOpen && list.folder" class="fas fa-fw fa-chevron-left"></i>
+          <span else class="text-gray-600 group-focus:text-gray-800">
+            {{totalTasks(list.sublists)}}
+          </span>
+
+          <div :class="{'pl-2' : list.folder}">
+            <i v-if="list.folder" class="fas fa-chevron-left norm fa-fw" :class="{ rotate : list.isOpen }"></i>
           </div>
+
+        </div>
+
       </li>
+      <div v-if="list.sublists && list.isOpen">
+        <ul v-for="sublist in list.sublists" :key="sublist.id" @click="selected = sublist.id">
+        <li tabindex="0" class="group flex justify-between items-center h-8 rounded-lg px-2 pl-8 my-1 cursor-pointer focus:bg-mana-100"
+        @click="rotate(sublist)">
+          <span class="text-gray-600 group-focus:font-sans-bold group-focus:text-gray-700 truncate">
+            <i class="fas fa-list-ul pr-3  text-gray-600  group-focus:text-mana-200"></i>
+            {{sublist.title}}
+          </span>
+          <div class="flex items-center pl-2">
+            <span class="text-gray-600 group-focus:text-gray-800">
+              {{sublist.tasks}}
+            </span>
+          </div>
+        </li>
+      </ul>
+
+      </div>
     </ul>
     
   </section>
@@ -67,6 +93,7 @@
     },
     data() {
       return {
+        a: 1,
         selected: undefined,
         active_el: 0,
         lists: [
@@ -76,7 +103,7 @@
             tasks: 3,
             folder: false,
             isOpen: false,
-            subList: false
+            sublists: []
           },
           {
             id: 1,
@@ -84,7 +111,7 @@
             tasks: 7,
             folder: false,
             isOpen: false,
-            subList: false
+            sublists: []
           },
           {
             id: 2,
@@ -92,43 +119,63 @@
             tasks: 18,
             folder: true,
             isOpen: false,
-            subList: false          
-            },
+            sublists: []  
+          },
           {
             id: 3,
             title: "Shopping Lists",
             tasks: 12,
             folder: true,
             isOpen: true,
-            subList: false
+            sublists: [
+              {
+                id: 0,
+                title: "Groceries",
+                tasks: 4,
+              },
+              {
+                id: 1,
+                title: "Hardware",
+                tasks: 12,
+              },
+            ]  
           },
-          {
-            id: 5,
-            title: "Groceries",
-            tasks: 4,
-            folder: false,
-            isOpen: false,
-            subList: true
-          },
-          {
-            id: 6,
-            title: "Hardware",
-            tasks: 12,
-            folder: false,
-            isOpen: false,
-            subList: true
-          },
+
         ]
       }
     },
     methods:{
       activate:function(el){
         this.active_el = el;
+      },
+      rotate(el) {
+        if(el.folder) el.isOpen = !el.isOpen;
+      },
+      totalTasks(lists) {
+        if(lists.length > 0){
+          return lists.reduce((prev, cur) => {
+            return prev + cur.tasks;
+          }, 0);
+        } else return "";
       }
+
+    },
+    computed: {
+
     },
   }
 </script>
 
 <style scoped>
+.norm{
+    -moz-transition: all .15s linear;
+    -webkit-transition: all .15s linear;
+    transition: all .15s linear;
+}
 
+.rotate{
+    -moz-transform:rotate(-90deg);
+    -webkit-transform:rotate(-90deg);
+    transform:rotate(-90deg);
+}
 </style>
